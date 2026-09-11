3b767dc752b5ae71cc4c7ab11be269ce00225cb2
Aman-Bam <amanbam604@gmail.com>
2026-09-11 20:30:22 +0530

    feat: initialize express application with CORS, cookie parsing, and transaction rate limiting

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://banking-system-updated.vercel.app"],
     credentials: true
 }))
 
