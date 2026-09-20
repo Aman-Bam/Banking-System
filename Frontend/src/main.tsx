95770139bb794699a17194a72c49470d25893302
Aman-Bam <amanbam604@gmail.com>
2026-09-20 17:25:55 +0530

    wrap RouterProvider with AuthProvider in main.tsx to fix TypeError cannot destructure user error

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,9 +4,12 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './app/queryClient'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </QueryClientProvider>
 )
